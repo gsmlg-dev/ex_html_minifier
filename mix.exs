e740aa1fb2ac3ac2f234b6d8872f2ba436877a9e
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-14 13:59:09 +0800

    chore: Fix ci.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HtmlMinifier.MixProject do
   def project do
     [
       app: :html_minifier,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       compilers: [:rustler] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
